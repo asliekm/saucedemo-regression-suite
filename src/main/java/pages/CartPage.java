b8e63327d00be197f1a9524dd2571d50258005f1
asliekm <asliekmekc@gmail.com>
2025-06-09 23:03:33 +0200

    refactor: Improve cart feature and overall POM design
    - Enhanced cart functionality and related tests
    - Refactored ProductsPage selectors to use data-test attributes
    - Improved Page Object Model structure
    - Various code cleanups and utility method improvements

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -27,6 +28,29 @@ public class CartPage extends BasePage {
     public void clickCheckoutButton() {
         waitAndClick(checkoutButton);
 
+    }
+    public boolean isProductInCart(String productName) {
+        return driver.findElements(By.xpath("//div[@class='inventory_item_name' and text()='" + productName + "']")).size() > 0;
+    }
+
+    public double getProductPrice(String productName) {
+        WebElement product = driver.findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + productName + "']/ancestor::div[contains(@class,'cart_item')]"));
+        String priceText = product.findElement(By.className("inventory_item_price")).getText();
+        return Double.parseDouble(priceText.replace("$", ""));
+    }
+
+    public boolean isRemoveButtonVisible(String productName) {
+        WebElement product = driver.findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + productName + "']/ancestor::div[contains(@class,'cart_item')]"));
+        return product.findElement(By.xpath(".//button[contains(text(),'Remove')]")).isDisplayed();
+    }
+
+    public void clickRemoveButton(String productName) {
+        WebElement product = driver.findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + productName + "']/ancestor::div[contains(@class,'cart_item')]"));
+        product.findElement(By.xpath(".//button[contains(text(),'Remove')]")).click();
+    }
+
+    public boolean isCartEmpty() {
+        return driver.findElements(By.className("cart_item")).isEmpty();
     }
 
 }
--- src/test/resources/features/cart.feature
+++ src/test/resources/features/cart.feature
@@ -1,21 +1,39 @@
-@regression
+@cart
 Feature: Cart Functionality
 
   Background:
-    Given I am on the login page
-    When I enter username "standard_user" and password "secret_sauce"
-    Then I should see the "products" page
+    Given I am logged in as a standard user
 
-  Rule: Cart should reflect added and removed items correctly
+  Rule: Adding products to the cart
 
-    Scenario Outline: Checkout with various user data
+    Scenario Outline: User can add a product to the cart and see correct details
       When I add the product "<product>" to the cart
       And I click the "cart" button
-      Then I should see the "Cart" page
-      When I click the "checkout" button
-      Then I should see the "Checkout: Your Information" page
+      Then I should see "<product>" in the cart
+      And The price for "<product>" should be displayed correctly
+      And The "Remove" button should be visible for "<product>"
 
       Examples:
         | product                  |
         | Sauce Labs Bike Light    |
         | Sauce Labs Fleece Jacket |
+
+  Rule: Removing products from the cart
+
+    Scenario Outline: User can remove a product from the cart
+      When I add the product "<product>" to the cart
+      And I click the "cart" button
+      And I remove the product "<product>" from the cart
+      Then The cart should be empty
+
+      Examples:
+        | product                  |
+        | Sauce Labs Bike Light    |
+        | Sauce Labs Fleece Jacket |
+
+  Rule: Cart state with no products
+
+    Scenario: Cart is empty if no product is added
+      When I click the "cart" button
+      Then The cart should be empty
+
